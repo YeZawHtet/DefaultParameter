9d766139b97154f93de1114881ea600fa0c1ec24
YeZawHtet <yezawhtet.web@gmail.com>
2023-08-12 17:10:44 +0630

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+        Run(500);
+    }
+
+    static void Run(int speed = 300)
+    {
+        Console.WriteLine("This is running with Speed "+ speed);
+    }
+}
